d5666f5fa41366975781f19ff672dfe1c2dc9e61
Shriya003 <sriyasri003@gmail.com>
2022-01-21 23:30:53 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Homepage from './component/homepage/homepage';
+import Login from './component/login/login';
+import Register from './component/register/register';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    {/*  <Homepage/> 
+        <Register/> */}
+    
+   
+     
+      <Login/>
+
     </div>
   );
 }
---
+++ src/component/homepage/homepage.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./homepage.css"
+
+const Homepage = ()=> {
+    return(
+
+        <div className="homepage">
+           <h1> hello homepage</h1>
+            <button>logout</button>
+
+        </div>
+    )
+}
+export default Homepage
