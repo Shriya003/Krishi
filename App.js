8d6d4e21264b899115ff6eef773b0ac99cd1a8c1
Shriya Srivastava <84763562+Shriya003@users.noreply.github.com>
2022-01-21 22:59:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Homepage from './component/homepage/homepage';
+import Login from './component/login/login';
+import Register from './component/register/register';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+    {/*  <Homepage/> 
+        <Register/> */}
+    
+   
+     
+      <Login/>
+
+    </div>
+  );
+}
+
+export default App;
